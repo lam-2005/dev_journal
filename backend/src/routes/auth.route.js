ba326661dd91f4e58fb46424361042477a5eaf98
lam-2005 <lam2k5phan@gmail.com>
2026-03-25 13:25:17 +0700

    login api implemented

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -3,5 +3,6 @@ import AuthController from "../controllers/auth.comtroller.js";
 const router = Router();
 
 router.post("/signup", AuthController.signup);
+router.post("/login", AuthController.login);
 
 export default router;
