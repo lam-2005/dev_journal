dd2c0d2eeb0e844a69859d7815e6ef3a1f4ee9ff
lam-2005 <lam2k5phan@gmail.com>
2026-04-14 15:54:35 +0700

    proxy fix

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 import env from "@/config/environment";
 
 const instance = axios.create({
-  baseURL:
-    env.NEXT_ENV === "development" ? "http://localhost:8080" : env.API_URL,
+  baseURL: env.NEXT_ENV === "development" ? "http://localhost:8080" : "/",
   withCredentials: true,
 });
 export default instance;
