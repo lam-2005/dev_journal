87d663d4ab0ae4df17a786fe72291561dfebe539
lam-2005 <lam2k5phan@gmail.com>
2026-04-14 16:12:50 +0700

    proxy fix

--- backend/src/lib/ultil.js
+++ backend/src/lib/ultil.js
@@ -11,7 +11,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "lax", // bắt buộc khi cross-domain
+    sameSite: "strict", // bắt buộc khi cross-domain
     path: "/",
     secure: NODE_ENV === "development" ? false : true,
   });
