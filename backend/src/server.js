cbf88d69635136983a74c19d8d7cf293a84ba488
lam-2005 <lam2k5phan@gmail.com>
2026-04-14 14:21:43 +0700

    fix bug and test

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,13 +9,7 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(
-  cors({
-    origin:
-      env.NODE_ENV === "development" ? "http://localhost:3000" : env.URL_CLIENT,
-    credentials: true,
-  }),
-);
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 
